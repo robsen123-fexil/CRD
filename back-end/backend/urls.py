6e0aa1f42b31421c7bc4e0f325cefe922e325f77
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-06 22:58:31 +0300

    bckend

---
+++ back-end/backend/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from user_api.views import SongView
+
+# import Routers conf from rest frameework
+from rest_framework import routers
+
+
+route = routers.DefaultRouter()
+route.register("", SongView, basename='Songview')
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(route.urls)),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
